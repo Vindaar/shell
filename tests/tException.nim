4c240232cd93fb7dccfbe7d7a0c555ed208da0da
Vindaar <basti90@gmail.com>
2020-12-26 12:07:32 +0100

    replace travis CI by Github Actions
    This replaces travis CI by github actions.
    
    It disables tests on Windows if they run actual commands (to be
    investigated #16).
    
    Also docs are only built on Linux.

--- tests/tException.nim
+++ tests/tException.nim
@@ -4,21 +4,22 @@ import ../shell
 import unittest
 import strutils
 
-suite "[shell]":
-  test "[exception] throw on invalid command":
-    try:
-      shell:
-        ls -l
-        ls -z
-    except ShellExecError:
-      let e = cast[ShellExecError](getCurrentException())
-      echo e.msg
-      echo "command was: ", e.cmd
-      assert e.cmd == "ls -z"
-      echo "executed in directory:", e.cwd
-      echo "return code: ", e.retcode
-      echo "error outpt: "
-      for l in e.errstr.split('\n'):
-        echo "  ", l
-    except:
-      assert false, "Execution must throw exception `ShellExecError`"
+when not defined(windows):
+  suite "[shell]":
+    test "[exception] throw on invalid command":
+      try:
+        shell:
+          ls -l
+          ls -z
+      except ShellExecError:
+        let e = cast[ShellExecError](getCurrentException())
+        echo e.msg
+        echo "command was: ", e.cmd
+        assert e.cmd == "ls -z"
+        echo "executed in directory:", e.cwd
+        echo "return code: ", e.retcode
+        echo "error outpt: "
+        for l in e.errstr.split('\n'):
+          echo "  ", l
+      except:
+        assert false, "Execution must throw exception `ShellExecError`"
