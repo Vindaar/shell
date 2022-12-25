de4d60c6aec7d019978d2b2fee01ee1bcd6fb762
Vindaar <basti90@gmail.com>
2022-12-25 12:31:06 +0100

    allow `send` without explicit `expect`

--- tests/tExpect.nim
+++ tests/tExpect.nim
@@ -4,10 +4,20 @@ import std / unittest
 # only run on linux due to the tExpect shell script (and not any platform bindings
 # of the `expect` support)
 when defined(linux):
-  var res = ""
-  shellAssign:
-    res = "./tests/tExpect.sh"
-    expect: "Your name?"
-    send: "Vindaar"
-  check res == """Hello world. Your name?
+  block:
+    var res = ""
+    shellAssign:
+      res = "./tests/tExpect.sh"
+      expect: "Your name?"
+      send: "Vindaar"
+    check res == """Hello world. Your name?
+Your name is Vindaar"""
+
+  block:
+    # now try without the `expect`
+    var res = ""
+    shellAssign:
+      res = "./tests/tExpect.sh"
+      send: "Vindaar"
+    check res == """Hello world. Your name?
 Your name is Vindaar"""
